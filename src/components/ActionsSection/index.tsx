ad58395152fcea4cdc17f08049e8f822b0860fb7
Lucas Nardon <lucaslnardon@gmail.com>
2022-02-17 05:56:34 -0300

    ✨ Update actions section

--- src/components/ActionsSection/index.tsx
+++ src/components/ActionsSection/index.tsx
@@ -1,6 +1,9 @@
 import { ActionCard } from "../../components/ActionCard";
 import styles from "./styles.module.css";
+
 import TransferIcon from "../../assets/transferIcon.png";
+import WalletIcon from "../../assets/walletIcon.png";
+import ContractIcon from "../../assets/contractIcon.png";
 
 export function ActionsSection({
   connectMetamask,
@@ -24,9 +27,19 @@ export function ActionsSection({
             onClick={() => alert("In Progress")}
           />
           <ActionCard
-            image={TransferIcon}
+            image={WalletIcon}
             label="Get NFTee Owner"
             onClick={() => alert("In Progress")}
+          />
+          <ActionCard
+            image={ContractIcon}
+            label="View Contract"
+            onClick={() =>
+              window.open(
+                "https://ropsten.etherscan.io/address/0x44AfB6114a02D987c252868405fB61488318d282",
+                "target=_blank"
+              )
+            }
           />
         </div>
       )}
