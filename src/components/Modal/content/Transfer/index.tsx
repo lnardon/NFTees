f330ce8ea8030e1e8936a617c6560d93f0602edb
Lucas Nardon <lucaslnardon@gmail.com>
2022-02-25 03:26:23 -0300

    💄 Transfer modal UI update

--- src/components/Modal/content/Transfer/index.tsx
+++ src/components/Modal/content/Transfer/index.tsx
@@ -4,30 +4,68 @@ import styles from "./styles.module.css";
 
 interface TransferProps {
   handleTransfer: (wallet: string, tokenId: number) => void;
+  handleClose: () => void;
 }
 
-const Transfer = ({ handleTransfer }: TransferProps) => {
+const Transfer = ({ handleTransfer, handleClose }: TransferProps) => {
   const [wallet, setWallet] = useState("");
   const [tokenId, setTokenId] = useState(0);
 
+  function hasContent(variable: string | number) {
+    if (typeof variable === "number") {
+      return variable > 0;
+    } else {
+      return variable.length > 0;
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Transfer ownership</h1>
-      <label htmlFor="tokenId">Type the NFTEE ID to be transfered</label>
+      <div className={styles.closeDiv}>
+        <img
+          onClick={handleClose}
+          src="https://cdn-icons-png.flaticon.com/512/463/463612.png"
+          alt="Close Icon"
+          className={styles.closeBtn}
+        />
+      </div>
+      <h1 className={styles.title}>Transfer ownership</h1>
+      {
+        <label
+          htmlFor="tokenId"
+          className={hasContent(tokenId) ? styles.label : styles.hiddenLabel}
+        >
+          Token ID:
+        </label>
+      }
       <input
         name="tokenId"
-        type="text"
-        placeholder="23"
+        type="number"
+        placeholder="NFTee ID"
         onChange={(e) => setTokenId(parseInt(e.target.value))}
+        className={styles.input}
       />
-      <label htmlFor="wallet">Paste the wallet to transfer the NFTEE</label>
+      {
+        <label
+          htmlFor="wallet"
+          className={hasContent(wallet) ? styles.label : styles.hiddenLabel}
+        >
+          Receiving address:
+        </label>
+      }
       <input
         name="wallet"
         type="text"
-        placeholder="0x44AfB6114a02D987c252868405fB61488318d282"
+        placeholder="Receiving address"
         onChange={(e) => setWallet(e.target.value)}
+        className={styles.input}
       />
-      <button onClick={() => handleTransfer(wallet, tokenId)}>Transfer</button>
+      <button
+        onClick={() => handleTransfer(wallet, tokenId)}
+        className={styles.btn}
+      >
+        Transfer
+      </button>
     </div>
   );
 };
